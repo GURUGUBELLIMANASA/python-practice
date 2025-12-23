7a43896a3ef03527432bf3be76c5de7ab8bf560f
GURUGUBELLIMANASA <gurugubellimanasa07@gmail.com>
2025-12-23 19:48:10 +0530

    add student_management system

--- student_management.py
+++ student_management.py
@@ -1,39 +1,41 @@
 #student management system using file handling
-FILE_NAME= "student.txt"
-def add_student();
-roll = input("enter the student roll no.")
-name= input("enter student name")
-Branch =input("enter student branch")
-student = {"Roll": roll,"Name":name, "Branch": branch}
-students.append(student)
+students = []
+def add_student():
+    roll = input("Enter the student roll no.")
+    name = input("Enter student name")
+    branch = input("Enter student branch")
+    student = {"Roll": roll,"Name": name, "Branch": branch}
+    students.append(student)
 
-def view_student();
-if not student:
-print("not student is found")
-return
+def view_students():
+  if not students:
+      print("not student is found")
+      return
 
-print("---student list---")
-for student in students:
-print{"Roll{"student['Roll']}|Name{student['name"]}|"Branch{student['branch']}"}
+  print("---Student list---")
+  for student in students:
+        print(f"Roll:{student["Roll"]}|Name:{student["name"]}|Branch:{student["branch"]}")
+      print()
 
- def main():
- while true:
- print("---student management system---")
- print('1. add the students")
- print("2. view the students")
- print("3. exit the program")
+def main():
+  while True:
+      print("---student management system---")
+      print("1. add the student")
+      print("2. view the studens")
+      print("3. exit the program")
 
- choice = input("enter the coice you want")
+      choice = input("enter the coice you want: ")
 
- if choice == '1':
-   add student()
-   elif choice == '2':
-   view_student()
-   elif choice =='3':
-   print("exit the program")
-   else
-   print("invalid choice, try again")
-
+      if choice == "1":
+        add student()
+      elif choice == "2":
+        view_student()
+      elif choice =="3:
+        print("exit the program")
+        break
+      else:
+        print("invalid choice, try again")
+main()
 
 
 
